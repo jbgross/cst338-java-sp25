06313a4ebe98cb0c24d4164949f5e975f4450488
Joshua B. Gross <gross.joshua.b@gmail.com>
2025-02-04 19:05:18 -0800

    added a new important file

---
+++ src/main/java/wk03/TestFile2.java
@@ -0,0 +1,7 @@
+package wk03;
+import java.io.File;
+public class TestFile2 {
+    public static void main(String[] args) {
+        System.out.println("This class is very important and does a lot of things");
+    }
+}
