0906b15e4a0b3ba2c3c4e37f0d34e211e22c9dbe
Joshua B. Gross <gross.joshua.b@gmail.com>
2025-02-04 19:54:54 -0800

    I have created a new file for no purpose other than showing how git branch and merge work

---
+++ src/main/java/wk03/ImportantClass.java
@@ -0,0 +1,7 @@
+package wk03;
+import java.util.ArrayList;
+public class ImportantClass {
+    public static void main(String[] args) {
+        System.out.println("this is a very important method");
+    }
+}
