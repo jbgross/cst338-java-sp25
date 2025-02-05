e49e00b15685f53ac8d565fedf443e58e1d7964d
Joshua B. Gross <gross.joshua.b@gmail.com>
2025-02-04 19:55:42 -0800

    removed unnecessary importa

--- src/main/java/wk03/ImportantClass.java
+++ src/main/java/wk03/ImportantClass.java
@@ -1,5 +1,5 @@
 package wk03;
-import java.util.ArrayList;
+
 public class ImportantClass {
     public static void main(String[] args) {
         System.out.println("this is a very important method");
