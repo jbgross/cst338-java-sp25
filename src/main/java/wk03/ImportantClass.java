45abfd50e3b0b967e4ee6638f7a0e709eafba886
Joshua B. Gross <gross.joshua.b@gmail.com>
2025-02-04 19:28:32 -0800

    added a file that is very important

---
+++ src/main/java/wk03/ImportantClass.java
@@ -0,0 +1,8 @@
+package wk03;
+import java.util.ArrayList;
+public class ImportantClass {
+    public static void main(String[] args) {
+        // this is a lie
+        System.out.println("this is a very important class that does a lot of stuff");
+    }
+}
