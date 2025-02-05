5fd54826cf6826db4ddb8ba590d0f59bc11e65cf
Joshua B. Gross <gross.joshua.b@gmail.com>
2025-02-04 19:33:24 -0800

    removed old class

--- src/main/java/wk03/ImportantClass.java
+++
@@ -1,8 +0,0 @@
-package wk03;
-
-public class ImportantClass {
-    public static void main(String[] args) {
-        // this is a lie
-        System.out.println("this is a very important class that does a lot of stuff");
-    }
-}
