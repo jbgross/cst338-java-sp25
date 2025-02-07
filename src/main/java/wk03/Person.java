8e3278831edb96285c5edf82d9829de0f0690cc1
Joshua B. Gross <gross.joshua.b@gmail.com>
2025-02-06 17:14:18 -0800

    created and stubbed out Person class

---
+++ src/main/java/wk03/Person.java
@@ -0,0 +1,42 @@
+package wk03;
+
+import java.time.LocalDate;
+
+public class Person {
+    private String name;
+    private LocalDate dateOfBirth;
+
+    public Person(String name, LocalDate dateOfBirth) {
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", dateOfBirth=" + dateOfBirth +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return false;
+    }
+}
